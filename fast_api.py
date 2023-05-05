a99ec58186dafe7af6cad59f8a0dda13ce015325
mapbobi-commit <69840883+mapbobi-commit@users.noreply.github.com>
2023-05-05 20:18:47 +0900

    Added error handling and requirements file

--- fast_api.py
+++ fast_api.py
@@ -1,9 +1,6 @@
-from typing import Annotated
-from fastapi import FastAPI, File, HTTPException
+from fastapi import FastAPI, UploadFile, HTTPException
 from pydantic import BaseModel
 from google.cloud import storage, bigquery
-from io import StringIO
-import csv
 
 
 class Querry(BaseModel):
@@ -15,14 +12,20 @@ client = bigquery.Client()
 
 
 @app.post("/uploadfile/{csv_file_id}")
-def create_upload_file(csv_file_id, csv_file: Annotated[bytes, File()]):
+def create_upload_file(csv_file_id, csv_file: UploadFile):
+    csv_content = csv_file.file.read()
+    print(csv_file.filename.split(".")[-1])
+
+    if csv_file.filename.split(".")[-1] != "csv":
+        raise HTTPException(status_code=404, detail="The file you chose is not a csv")
+
     # Uploads file to Google Cloud Storage
     storage_client = storage.Client()
     bucket_name = storage_client.bucket("api_tester")
     blob = bucket_name.blob(csv_file_id)
     generation_match_precondition = 0
 
-    blob.upload_from_string(csv_file)
+    blob.upload_from_string(csv_content)
 
     # Constructs the file in BigQuery
     table_id = f"omniproject-51.api_testing.{csv_file_id}"
@@ -34,7 +37,13 @@ def create_upload_file(csv_file_id, csv_file: Annotated[bytes, File()]):
     )
     uri = f"gs://api_tester/{csv_file_id}"
     load_job = client.load_table_from_uri(uri, table_id, job_config=job_config)
-    load_job.result()
+
+    try:
+        load_job.result()
+    except:
+        raise HTTPException(
+            status_code=404, detail="The csv file has incosistencies or is not a csv"
+        )
     client.get_table(table_id)
 
 
@@ -42,7 +51,10 @@ def create_upload_file(csv_file_id, csv_file: Annotated[bytes, File()]):
 def querry(item: Querry):
     query = item.querry
     querry_job = client.query(query)
-    data = querry_job.result()
+    try:
+        data = querry_job.result()
+    except:
+        raise HTTPException(status_code=404, detail="The querry failed")
     rows = list(data)
 
     return rows
